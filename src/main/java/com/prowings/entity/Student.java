2c8fb308862d47aea129198e58b99bf506328a6c
karan <karanbhore64@gmail.com>
2023-06-24 21:01:07 +0530

    annotationns in hibernate

---
+++ src/main/java/com/prowings/entity/Student.java
@@ -0,0 +1,77 @@
+package com.prowings.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+
+	@Column
+	int id;
+
+	@Column(name = "STD_ROLL")
+	int roll;
+
+	@Column
+	String name;
+
+	@Column(name = "STD_ADDRESS")
+	String address;
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int id, int roll, String name, String address) {
+		super();
+		this.id = id;
+		this.roll = roll;
+		this.name = name;
+		this.address = address;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", roll=" + roll + ", name=" + name + ", address=" + address + "]";
+	}
+
+}
---
+++ src/main/java/com/prowings/main/TestHibernateApp.java
@@ -0,0 +1,36 @@
+package com.prowings.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.prowings.entity.Student;
+
+public class TestHibernateApp {
+
+	public static void main(String[] args) {
+
+		Student std1 = new Student();
+
+		std1.setRoll(25);
+		std1.setName("Aman");
+		std1.setAddress("Pune");
+
+		Configuration conf = new Configuration();
+		conf.configure();
+
+		SessionFactory sf = conf.buildSessionFactory();
+		Session session = sf.openSession();
+
+		Transaction txn = session.beginTransaction();
+
+		session.save(std1);
+
+		txn.commit();
+
+		System.out.println("student object saved to database successfully!!");
+		session.close();
+
+	}
+}
